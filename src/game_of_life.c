4ae2c922c3d19b5074bf9888d27a3c3a2747d0dd
Authentic Kenna <authentk@ox-h1.msk.21-school.ru>
2025-07-06 15:58:29 +0300

    All

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <ncurses.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #define N 25
@@ -11,34 +11,36 @@ void clean_memory_for_matrix(int ***matrix);
 
 void otrisovka(int **matrix);
 void change_position(int **matrix);
-int alive(int x,int y, int **matrix);
+int alive(int x, int y, int **matrix);
 int check_count_alive(int x, int y, int **matrix);
 
 void change_flag(char input, int *flag);
-void client_input(char *input); //то что пользователь ввел
+void client_input(char *input);
 void change_speed(int *delay, char input);
 
 int main(void) {
     int **matrix = NULL;
     memory_for_matrix(&matrix);
     start_position(matrix);
-    //initscr();
-    if (freopen("/dev/tty", "r", stdin)) initscr();
-    
-    nodelay(stdscr, TRUE); //неблокирующий режим (не останавливает работу программы пока гетч ждет)
-    cbreak(); //оставляем буферизацию строк (все отправляется программе сразу после нажатия без энтер)
-    noecho(); //отключаем вывод нажатых строк
 
-    int flag = 0;  //пробел и наличие живых клеток
+    if (freopen("/dev/tty", "r", stdin)) initscr();
+
+    nodelay(stdscr, TRUE);  // неблокирующий режим (не останавливает работу программы пока гетч ждет)
+    cbreak();  // оставляем буферизацию строк (все отправляется программе сразу после нажатия без энтер)
+    noecho();  // отключаем вывод нажатых строк
+
+    int flag = 0;  // пробел и наличие живых клеток
     char input;
     int delay = 100;
+
     while (flag == 0) {
-        change_position(matrix);
         otrisovka(matrix);
         refresh();
         client_input(&input);
+        change_speed(&delay, input);
         change_flag(input, &flag);
-        napms(delay);//задержка чтобы слишком много выводов не было
+        change_position(matrix);
+        napms(delay);  // задержка чтобы слишком много выводов не было
     }
     clean_memory_for_matrix(&matrix);
     endwin();
@@ -46,28 +48,26 @@ int main(void) {
 
 void memory_for_matrix(int ***matrix) {
     *matrix = malloc(sizeof(int *) * N);
-    int *values_array = malloc(sizeof(int ) * N * M);
+    int *values_array = malloc(sizeof(int) * N * M);
     for (int i = 0; i < N; i++) {
         (*matrix)[i] = values_array + i * M;
     }
-
 }
 
 void clean_memory_for_matrix(int ***matrix) {
     if (*matrix != NULL) {
         if ((*matrix)[0] != NULL) {
-        free((*matrix)[0]);
+            free((*matrix)[0]);
         }
         free(*matrix);
     }
-    
 }
 
 void start_position(int **matrix) {
     char c;
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
-            scanf("%c",&c);
+            scanf("%c", &c);
             if (c == '0') {
                 matrix[i][j] = 0;
             } else if (c == '1') {
@@ -80,7 +80,7 @@ void start_position(int **matrix) {
 
 void otrisovka(int **matrix) {
     clear();
-    for (int j = 0; j < M + 2; j ++) {
+    for (int j = 0; j < M + 2; j++) {
         printw("_");
     }
     printw("\n");
@@ -88,15 +88,15 @@ void otrisovka(int **matrix) {
         for (int j = 0; j < M + 2; j++) {
             if (j == 0 || j == M + 1) {
                 printw("|");
-            } else if ( matrix[i][j - 1] == 1) {
+            } else if (matrix[i][j - 1] == 1) {
                 printw("O");
             } else {
                 printw(" ");
-            } 
+            }
         }
         printw("\n");
     }
-    for (int j = 0; j < M + 2; j ++) {
+    for (int j = 0; j < M + 2; j++) {
         printw("_");
     }
     printw("\n");
@@ -116,15 +116,15 @@ void change_flag(char input, int *flag) {
 }
 
 void change_speed(int *delay, char input) {
-    if (input == 'A' && *delay - 100 >= 100) {
-        *delay -= 500;
+    if ((input == 'a' || input == 'A') && *delay - 25 >= 50) {
+        *delay -= 25;
     }
-    if (input == 'Z') {
-        *delay += 500;
+    if (input == 'z' || input == 'Z') {
+        *delay += 25;
     }
 }
 
-int alive(int x,int y, int **matrix){
+int alive(int x, int y, int **matrix) {
     int check = 0;
     x = (x + N) % N;
     y = (y + M) % M;
@@ -132,47 +132,51 @@ int alive(int x,int y, int **matrix){
     if (matrix[x][y] == 1) {
         check = 1;
     }
-    
+
     return check;
 }
 
-int check_count_alive(int x, int y, int **matrix){
+int check_count_alive(int x, int y, int **matrix) {
     int count = 0;
 
     for (int dy = -1; dy <= 1; dy++) {
         for (int dx = -1; dx <= 1; dx++) {
-            if (dx == 0 && dy == 0) continue;  
-            if (alive(x + dx, y + dy, matrix)) {
-                count++;
+            if (!(dx == 0 && dy == 0)) {
+                if (alive(x + dx, y + dy, matrix)) {
+                    count++;
+                }
             }
         }
     }
     return count;
 }
 
-void change_position(int **matrix){
+void change_position(int **matrix) {
     int **matrix_memory = NULL;
     memory_for_matrix(&matrix_memory);
 
-    for (int i = 0;i<N;i++){
-        for(int j = 0;j<M;j++){
-            int count = check_count_alive(i, j,matrix);
-            if ((count<2 || count>3) && matrix[i][j]==1){
-                matrix_memory[i][j] = 0;
-            }
-            if (count == 3 && matrix[i][j]==0){
-                matrix_memory[i][j]=1;
-            }
-            if ((count==2 || count == 3)&&matrix[i][j]==1){
-                matrix_memory[i][j] = 1;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            int count = check_count_alive(i, j, matrix);
+            if (matrix[i][j] == 1) {
+                if (count == 2 || count == 3) {
+                    matrix_memory[i][j] = 1;
+                } else {
+                    matrix_memory[i][j] = 0;
+                }
+            } else {
+                if (count == 3) {
+                    matrix_memory[i][j] = 1;
+                } else {
+                    matrix_memory[i][j] = 0;
+                }
             }
         }
     }
-    for (int i = 0;i<N;i++){
-        for(int j = 0;j<M;j++){
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
             matrix[i][j] = matrix_memory[i][j];
         }
     }
     clean_memory_for_matrix(&matrix_memory);
-
 }
